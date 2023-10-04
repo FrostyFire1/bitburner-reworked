93dbd7449bb259b63805afc826d531dc8b3b2149
FrostyFire1 <66720515+FrostyFire1@users.noreply.github.com>
2023-10-04 22:47:39 +0200

    implement getPrepInfo(), remove useless constants

--- src/rework/batcher.tsx
+++ src/rework/batcher.tsx
@@ -1,33 +1,31 @@
-import { NS } from "/NetscriptDefinitions";
-import {getServerList, batchPotential, isPrepped} from "./rework/lib";
+import { NS } from "@ns";
+import {getServerList, batchPotential, isPrepped, SEC_DEC_WEAKEN} from "./rework/lib";
 let STEAL_PERCENTAGE = 0.5;
-const SEC_DEC_WEAKEN = 0.05;
+
 const TARGET = "foodnstuff";
 
 export async function main(ns: NS) {
     ns.disableLog('ALL'); ns.clearLog(); ns.tail();
     const targets = getServerList(ns)
-    .sort((a,b) => return batchPotential(ns, b) - batchPotential(ns, a));
+    .sort((a,b) => {return batchPotential(ns, b) - batchPotential(ns, a);});
     let toPrep = targets.filter(a => !isPrepped(ns, a));
     ns.print(toPrep);    
 }
 
 function simulateBatch(ns: NS, server: string){
-    const SEC_PER_HACK = ns.hackAnalyzeSecurity(1,server);
-    const SEC_PER_GROW = ns.growthAnalyzeSecurity(1,server,1);
 
     let sim = ns.getServer(server);
     const player = ns.getPlayer();
     sim.hackDifficulty = sim.minDifficulty;
     sim.moneyAvailable = sim.moneyMax;
-
+    
     const hp = ns.formulas.hacking.hackPercent(sim, player);
     const hthreads = Math.ceil(STEAL_PERCENTAGE / hp);
     sim.moneyAvailable = sim.moneyMax * STEAL_PERCENTAGE;
-    sim.hackDifficulty += Math.min(hthreads*SEC_PER_HACK, 100-sim.hackDifficulty);
+    sim.hackDifficulty += Math.min(ns.hackAnalyzeSecurity(hthreads,server), 100-sim.hackDifficulty);
 
     const gthreads = ns.formulas.hacking.growThreads(sim, player, sim.moneyMax);
-    sim.hackDifficulty += Math.min(gthreads*SEC_PER_GROW,100-sim.hackDifficulty);
+    sim.hackDifficulty += Math.min(ns.growthAnalyzeSecurity(gThreads, server,1),100-sim.hackDifficulty);
 
     const wThreads = Math.ceil((sim.hackDifficulty - sim.minDifficulty) / SEC_DEC_WEAKEN);
 
--- src/rework/lib.ts
+++ src/rework/lib.ts
@@ -1,5 +1,6 @@
 import { NS } from "@ns";
 
+
 export function getServerList(ns: NS): string[] {
     let servers = new Set(ns.scan("home"));
     servers.forEach(server => {
@@ -7,18 +8,22 @@ export function getServerList(ns: NS): string[] {
     });
     return Array.from(servers);
 }
-
+//-----------------------------------------BATCH RELATED FUNCTIONS----------------------------------------------------------------------------
+/**
+ * Calculates the batch potential of a server. Higher batch potential means the server is a better target.
+ * @param ns The ns interface
+ * @param target hostname of the target server
+ * @returns The batch potential of the server.
+ */
 export function batchPotential(ns: NS, target: string): number {
     let server = ns.getServer(target);
     server.hackDifficulty = server.minDifficulty;
-    const maxMoney = server.moneyMax;
     let difficulty = server.hackDifficulty;
     if(ns.fileExists('Formulas.exe')){
-        if (!server.hasAdminRights) difficulty = Infinity;
         else difficulty = ns.formulas.hacking.weakenTime(server, ns.getPlayer()) / ns.formulas.hacking.hackChance(server, ns.getPlayer());
     }
     else if(server.requiredHackingSkill > ns.getHackingLevel() / 2) return 0;
-    return maxMoney / difficulty;
+    return server.moneyMax / difficulty;
 }
 
 
@@ -27,6 +32,35 @@ export function isPrepped(ns: NS, server: string): boolean{
     return info.moneyAvailable === info.moneyMax && info.hackDifficulty === info.minDifficulty;
 }
 
+/**
+ * Brings the given server to maximum money and minimum security.
+ * @param ns The ns interface
+ * @param hostname Hostname of the server you want to prep
+ */
+export function prepServer(ns: NS, hostname: string) {
+    const [gThreads, wThreads] = getPrepInfo(ns, hostname);
+
+}
+
+/**
+ * Get the amount of grow and weaken threads needed to bring a server to maximum money and minimum security.
+ * @remarks This function requires 'Formulas.exe' to be present on your home server
+ * @param ns The ns interface
+ * @param server Hostname of the server you want to prep
+ * @returns 2 element array containing the required amount of grow and weaken threads.
+ */
+function getPrepInfo(ns: NS, hostname: string): number[] {
+    const sInfo = ns.getServer(hostname);
+    const [curMoney, maxMoney] = [sInfo.moneyAvailable, sInfo.moneyMax];
+    const multiplier = maxMoney / curMoney;
+
+    const gThreads = ns.growthAnalyze(hostname, multiplier, 1);
+
+    const secIncrease = Math.min(100 - ns.gethostnameSecurityLevel(hostname), ns.growthAnalyzeSecurity(gThreads, hostname, 1));
+    const wThreads = Math.ceil(secIncrease / ns.weakenAnalyze(1,hostname,1));
+    return [gThreads, wThreads];
+}
+//-----------------------------------------OTHER----------------------------------------------------------------------------
 export async function main(ns: NS): Promise<void> {
     ns.disableLog('ALL'); ns.tail(); ns.clearLog();
     ns.print(getServerList(ns));
